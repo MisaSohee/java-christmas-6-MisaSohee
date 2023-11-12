1a84ab75f281dd01b43f224f3c54f659ceecfd2c
MisaSohee <sohee5951@naver.com>
2023-11-12 20:35:49 +0900

    feat(OutputView): 이벤트 주의 사항 출력 기능 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -4,6 +4,7 @@ public class Application {
     public static void main(String[] args) {
 
         OutputView outputView = new OutputView();
+        outputView.printEventGuidelines();
         outputView.printMenu();
         outputView.printMessage();
 
