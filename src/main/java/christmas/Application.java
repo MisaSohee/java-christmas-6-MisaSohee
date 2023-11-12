e008dce6092c9e7120f991da96ca4bc811cf457c
MisaSohee <sohee5951@naver.com>
2023-11-12 19:27:18 +0900

    feat(InputView): 식당 방문 날짜 입력 기능 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,6 +2,7 @@ package christmas;
 
 public class Application {
     public static void main(String[] args) {
-
+        InputView inputView = new InputView();
+        int date = inputView.readDate();
     }
 }
---
+++ src/main/java/christmas/InputView.java
@@ -0,0 +1,36 @@
+package christmas;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class InputView {
+    public int readDate() {
+        while (true){
+            try {
+                System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+                int date = parseDate(Console.readLine());
+                validateDate(date);
+                return date;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            }
+        }
+    }
+
+    private int parseDate(String input) {
+        try{
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateDate(int date) {
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
