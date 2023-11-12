da943c3f48795245f5f34cbac43bd1c7819351f2
MisaSohee <sohee5951@naver.com>
2023-11-12 20:32:53 +0900

    feat(Menu): 메뉴판 생성 및 출력

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,6 +2,11 @@ package christmas;
 
 public class Application {
     public static void main(String[] args) {
+
+        OutputView outputView = new OutputView();
+        outputView.printMenu();
+        outputView.printMessage();
+
         InputView inputView = new InputView();
         int date = inputView.readDate();
     }
---
+++ src/main/java/christmas/Menu.java
@@ -0,0 +1,27 @@
+package christmas;
+
+public enum Menu {
+    YANGSONGISOUP("양송이수프", 6000,Category.APPETIZER),
+    TAPAS("타파스", 5500, Category.APPETIZER),
+    CAESARSALAD("시저샐러드", 8000, Category.APPETIZER),
+    TBONESTEAK("티본스테이크", 55000, Category.MAIN),
+    BBQRIB("바비큐립", 54000, Category.MAIN),
+    SEAFOODPASTA("해산물파스타", 35000, Category.MAIN),
+    CHRISTMASPASTA("크리스마스파스타", 25000, Category.MAIN),
+    CHOCOCAKE("초코케이크", 15000, Category.DESSERT),
+    ICECREAM("아이스크림", 5000, Category.DESSERT),
+    ZEROCOLA("제로콜라", 3000, Category.DRINK),
+    REDWINE("레드와인", 60000, Category.DRINK),
+    CHAMPAGNE("샴페인", 25000, Category.DRINK);
+
+
+    private final String name;
+    private final int price;
+    private final Category category;
+
+    Menu(String name, int price, Category category){
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
