510dfee6ed68bffaa97871be3b461a11ab33be6c
MisaSohee <sohee5951@naver.com>
2023-11-13 19:56:05 +0900

    feat(OrderValidator): 메뉴 개수 범위에 대한 검증 기능 추가

--- src/main/java/christmas/OrderValidator.java
+++ src/main/java/christmas/OrderValidator.java
@@ -5,7 +5,13 @@ public class OrderValidator {
         try {
             Menu.from(menuName);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("[ERROR] 메뉴판에 없는 메뉴입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public static void validateQuantity(int quantity) {
+        if (quantity < 1 || quantity > 20) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 }
