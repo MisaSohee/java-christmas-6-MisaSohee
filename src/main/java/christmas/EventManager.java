5f68509d54c5b1d3010dc5989745aff91362c9e5
MisaSohee <sohee5951@naver.com>
2023-11-14 00:13:10 +0900

    feat(EventManager): 평일 할인 계산 기능 추가

--- src/main/java/christmas/EventManager.java
+++ src/main/java/christmas/EventManager.java
@@ -1,9 +1,23 @@
 package christmas;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class EventManager {
     public static int applyDdayDiscount(int totalPrice, int orderDate) {
         if(orderDate <= 25) {
             int discount = 1000 + (orderDate - 1) * 100;
+            return totalPrice - discount;
+        }
+        return totalPrice;
+    }
+
+    public static int applyWeekdayDiscount(int totalPrice, int orderDate, int numberOfDesserts) {
+        LocalDate date = LocalDate.of(2023, 12, orderDate);
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+
+        if (dayOfWeek != DayOfWeek.FRIDAY && dayOfWeek != DayOfWeek.SATURDAY) {
+            int discount = 2023 * numberOfDesserts;
             return totalPrice - discount;
         }
         return totalPrice;
