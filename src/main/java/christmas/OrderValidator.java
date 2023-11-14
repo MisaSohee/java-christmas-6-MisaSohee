57c6535ce98f5f52fa6986330bc9db7d054978ae
MisaSohee <sohee5951@naver.com>
2023-11-15 02:40:42 +0900

    refactor(OrderValidater): validateNotOnlyDrinks 메서드의 예외처리 메세지 변경

--- src/main/java/christmas/OrderValidator.java
+++ src/main/java/christmas/OrderValidator.java
@@ -67,6 +67,6 @@ public class OrderValidator {
                 return;
             }
         }
-        throw new IllegalArgumentException("[ERROR] 음료만 주문하실 수 없습니다. 다른 메뉴를 추가해주세요.");
+        throw new IllegalArgumentException("[ERROR] 음료만 주문 시, 주문할 수 없습니다. 다른 메뉴를 추가해주세요.");
     }
 }
